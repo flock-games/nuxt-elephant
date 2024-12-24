51e1816aaffbdf8c3c8fb6895252148b59bfdcbd
Tyler Dawson <tddawson@gmail.com>
2024-12-23 16:25:59 -0800

    Loading of games

--- pages/[slug].vue
+++ pages/[slug].vue
@@ -1,43 +1,34 @@
 <script setup lang="ts">
 import type { SanityDocument } from "@sanity/client";
-import imageUrlBuilder from "@sanity/image-url";
-import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
-const POST_QUERY = groq`*[_type == "post" && slug.current == $slug][0]`;
+const GAME_QUERY = groq`*[
+  _type == "game" && _id == $slug
+][0]{_id, name}`;
+// const POST_QUERY = groq`*[_type == "post" && slug.current == $slug][0]`;
 const { params } = useRoute();
 
-const { data: post } = await useSanityQuery<SanityDocument>(POST_QUERY, params);
-const { projectId, dataset } = useSanity().client.config();
-const urlFor = (source: SanityImageSource) =>
-  projectId && dataset
-    ? imageUrlBuilder({ projectId, dataset }).image(source)
-    : null;
-
-const test = "hello";
-
-console.log(test);
+const { data: game } = await useSanityQuery<SanityDocument>(GAME_QUERY, params);
+console.log(game);
 </script>
 
 <template>
   <main
-    v-if="post"
     class="container mx-auto min-h-screen max-w-3xl p-8 flex flex-col gap-4"
   >
-    <a href="/" class="hover:underline">&larr; Back to posts</a>
-    <img
-      v-if="post.image"
-      :src="urlFor(post.image).width(550).height(310).url()"
-      :alt="post?.title"
-      class="aspect-video rounded-xl"
-      width="550"
-      height="310"
-    />
-    <h1 v-if="post.title" class="text-4xl font-bold mb-8">{{ post.title }}</h1>
-    <div class="prose">
-      <p v-if="post.publishedAt">
-        Published: {{ new Date(post.publishedAt).toLocaleDateString() }}
-      </p>
-      <SanityContent v-if="post.body" :blocks="post.body" />
+    <a href="/" class="hover:underline">&larr; Back to games</a>
+    <div v-if="game?._id">
+      <h1 v-if="game.name" class="text-4xl font-bold mb-8">
+        {{ game.name }}
+      </h1>
+      <!-- <div class="prose">
+        <p v-if="game.publishedAt">
+          Published: {{ new Date(game.publishedAt).toLocaleDateString() }}
+        </p>
+        <SanityContent v-if="game.body" :blocks="post.body" />
+      </div> -->
+    </div>
+    <div v-else>
+      <h1 class="text-4xl font-bold mb-8">No game found.</h1>
     </div>
   </main>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,49 @@
 <script setup lang="ts">
 import type { SanityDocument } from "@sanity/client";
 
-const POSTS_QUERY = groq`*[
-  _type == "post"
-  && defined(slug.current)
-]|order(publishedAt desc)[0...12]{_id, title, slug, publishedAt}`;
+const GAMES_QUERY = groq`*[
+  _type == "game"
+]{_id, name}`;
 
-const { data: posts } = await useSanityQuery<SanityDocument[]>(POSTS_QUERY);
+const USERS_QUERY = groq`*[
+  _type == "user"
+]| order(order asc){_id, name, order}`;
+
+const GIFTS_QUERY = groq`*[
+  _type == "gift"
+]{_id, url, owner->{name}}`;
+
+const { data: games } = await useSanityQuery<SanityDocument[]>(GAMES_QUERY);
+const { data: users } = await useSanityQuery<SanityDocument[]>(USERS_QUERY);
+const { data: gifts } = await useSanityQuery<SanityDocument[]>(GIFTS_QUERY);
+console.log(gifts);
 </script>
 
 <template>
   <main class="container mx-auto min-h-screen max-w-3xl p-8">
-    <h1 class="text-4xl font-bold mb-8">Posts</h1>
-    <ul class="flex flex-col gap-y-4">
-      <li v-for="post in posts" :key="post._id" class="hover:underline">
-        <nuxt-link :to="`/${post.slug.current}`">
-          <h2 class="text-xl font-semibold">{{ post.title }}</h2>
-          <p>{{ new Date(post.publishedAt).toLocaleDateString() }}</p>
-        </nuxt-link>
+    <h1 class="text-4xl font-bold mb-8">Games</h1>
+    <ul>
+      <li v-for="game in games" :key="game._id">
+        <h2 class="text-xl font-semibold">
+          <a :href="'/' + game._id">{{ game.name }}</a>
+        </h2>
       </li>
     </ul>
+    <!-- <ul class="flex flex-col gap-y-4">
+      <li v-for="user in users" :key="user._id">
+        <h2 class="text-xl font-semibold">{{ user.name }}</h2>
+        <p>{{ new Date(user.order).toLocaleDateString() }}</p>
+      </li>
+    </ul>
+
+    <h1 class="text-4xl font-bold mb-8">Gifts</h1>
+    <ul class="flex flex-col gap-y-4">
+      <li v-for="(gift, index) in gifts" :key="gift._id">
+        <h2 class="text-xl font-semibold">
+          <a :href="gift.url">{{ index }}</a>
+        </h2>
+        <p>{{ gift.owner?.name }}</p>
+      </li>
+    </ul> -->
   </main>
 </template>
